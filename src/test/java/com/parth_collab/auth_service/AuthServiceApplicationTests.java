b53be501a823575554c0dc79cb3bd2f7ef0f10cb
parth9906 <parth.sachan.parth@gmail.com>
2025-10-06 19:15:50 +0530

    first commit

---
+++ src/test/java/com/parth_collab/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parth_collab.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
